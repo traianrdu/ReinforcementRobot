70ba50b503aa11e5b91c9ef9d5c9c170b7c7912a
traianrdu <traian.plosca@gmail.com>
2021-03-11 00:30:52 +0200

    save points to file

--- GUI/drawingToolV2.py
+++ GUI/drawingToolV2.py
@@ -1,6 +1,8 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color, Line, Ellipse
+from Utility import algebra
+from Utility.vector2 import Vector2
 
 
 class DrawingToolV2(Screen, Widget):
@@ -10,6 +12,8 @@ class DrawingToolV2(Screen, Widget):
         self.last_point_y = None
         self.first_point_x = None
         self.first_point_y = None
+        self.d = []
+        self.check = None  # check if touch down was on the screen
 
     def on_touch_down(self, touch):
         if Widget.on_touch_down(self, touch):  # used to press buttons on screen
@@ -21,32 +25,49 @@ class DrawingToolV2(Screen, Widget):
             self.last_point_y = touch.y
             self.first_point_x = touch.x
             self.first_point_y = touch.y
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
-            print("mouse donw:", touch.pos)
-            print(self.last_point_x)
+            touch.ud['line'] = Line(points=(touch.x, touch.y))  # shows line
+            print("mouse down:", touch.pos)
+            self.d.append(Vector2(self.first_point_x, self.first_point_y))  # append to list
+            self.check = 1  # check completed
 
     def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
-        self.last_point_x = touch.x
+        touch.ud['line'].points += [touch.x, touch.y]   # shows line
+        self.last_point_x = touch.x     # saves last points
         self.last_point_y = touch.y
+        self.d.append(Vector2(self.last_point_x, self.last_point_y))    # append to list
 
     def on_touch_up(self, touch):
-        if self.last_point_x < self.first_point_x:
-            # Left
-            if self.last_point_y < self.first_point_y:
-                # Down
-                print("Left Down")
+        if self.check == 1:  # only if drawing mode
+            self.check = None   # reset the check
+            if self.last_point_x < self.first_point_x:
+                # Left
+                if self.last_point_y < self.first_point_y:
+                    # Down
+                    print("Left Down")
+                    touch.ud['line'].points += [self.first_point_x, self.first_point_y]
+                    #dist = algebra.findD(self.last_point_x, self.first_point_x, self.last_point_y, self.first_point_y)
+                    #while self.last_point_x != self.first_point_x and self.last_point_y != self.first_point_y:
+                else:
+                    # Up
+                    print("Left Up")
+                    touch.ud['line'].points += [self.first_point_x, self.first_point_y]
             else:
-                # Up
-                print("Left Up")
-        else:
-            # Right
-            if self.last_point_y < self.first_point_y:
-                # Down
-                print("Right Down")
-            else:
-                # Up
-                print("Right Up")
+                # Right
+                if self.last_point_y < self.first_point_y:
+                    # Down
+                    print("Right Down")
+                    touch.ud['line'].points += [self.first_point_x, self.first_point_y]
+                else:
+                    # Up
+                    print("Right Up")
+                    touch.ud['line'].points += [self.first_point_x, self.first_point_y]
+            file = open("GUI/SaveFile/file.txt", "w+")
+            for i in self.d:
+                print(i.x, i.y)
+                file.write(str(i.x)+" ")
+                file.write(str(i.y)+'\n')
+            file.close()
 
     def save(self):
-        print(self.last_point_x)
+        """Saves the data in the json"""
+        print(self.d)
--- Utility/algebra.py
+++ Utility/algebra.py
@@ -1,6 +1,12 @@
 import math
 
 
+def findD(xA, xB, yA, yB):
+    """Finds the distance between 2 points"""
+    d = math.sqrt(pow(xB - xA, 2) + pow(yB-yA, 2))
+    return d
+
+
 def findX(distance, xA, yA, yB):
     """find the xB"""
     xB = xA + math.sqrt(pow(distance, 2) - pow(yB-yA, 2))
