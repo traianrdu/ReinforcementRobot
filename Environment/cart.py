27abe98eed41f2639f0ca7fb82a088381936d9d3
Traian <traian.plosca@gmail.com>
2022-03-11 00:42:52 +0200

    add: NE, NW, SE, SW movements & render

--- Environment/cart.py
+++ Environment/cart.py
@@ -15,24 +15,41 @@ class Cart(pygame.sprite.Sprite):
         self.image.fill(self.color)  # sets the color
         self.rect = self.image.get_rect()  # creates the rectangular shape of the car
 
-        self.w = False
-        self.a = False
-        self.s = False
-        self.d = False
+        self.N = False  # North
+        self.W = False  # West
+        self.S = False  # South
+        self.E = False  # East
+        self.NE = False  # North East
+        self.NW = False  # North West
+        self.SE = False  # South East
+        self.SW = False  # South West
 
     def render(self, screen):
         """Renders the car on the first run"""
-        screen.blit(self.image, (self.coordinates.x, self.coordinates.y))
+        screen.blit(self.image, (self.coordinates.x, self.coordinates.y))   # draw image on coordinates
+        self.reset_movement()   # reset actions
 
     def move(self):
         """Move function"""
-        if self.w:
+        if self.N:
             self.coordinates.y -= 0.1
-        elif self.a:
+        elif self.W:
             self.coordinates.x -= 0.1
-        elif self.s:
+        elif self.S:
             self.coordinates.y += 0.1
-        elif self.d:
+        elif self.E:
+            self.coordinates.x += 0.1
+        elif self.NW:
+            self.coordinates.y -= 0.1
+            self.coordinates.x -= 0.1
+        elif self.NE:
+            self.coordinates.y -= 0.1
+            self.coordinates.x += 0.1
+        elif self.SW:
+            self.coordinates.y += 0.1
+            self.coordinates.x -= 0.1
+        elif self.SE:
+            self.coordinates.y += 0.1
             self.coordinates.x += 0.1
 
     def collide_box(self):
@@ -40,7 +57,11 @@ class Cart(pygame.sprite.Sprite):
 
     def reset_movement(self):
         """Reset movement actions"""
-        self.w = False
-        self.a = False
-        self.s = False
-        self.d = False
+        self.N = False
+        self.W = False
+        self.S = False
+        self.E = False
+        self.NE = False
+        self.NW = False
+        self.SE = False
+        self.SW = False
