fba7fafe4f25ab359be1ce25c194563dddec431e
Traian <traian.plosca@gmail.com>
2022-04-28 23:35:47 +0300

    fix: return step number

--- Environment/training_env.py
+++ Environment/training_env.py
@@ -38,13 +38,13 @@ class RiCart:
         if self.cart.did_collide():  # if we detect collide
             reward = -10
             game_over = True
-            return reward, game_over, self.score
+            return reward, game_over, self.score, self.n_step
 
         if self.cart.did_follow(self.cart_old_coordinates, self.leader_old_coordinates,
                                 Vector2(self.map.leader.coordinates.x, self.map.leader.coordinates.y)):  # if the cart follows the leader
             reward = 10  # we get extra reward
             self.score += 1  # increase score
-            return reward, game_over, self.score
+            return reward, game_over, self.score, self.n_step
 
         # else it just did move one step without collision
         return reward, game_over, self.score, self.n_step
